da064a5ab3ccfda83035680b7672846653f44420
oyinkansola <adebayoalameen88@gmail.com>
2023-02-10 22:25:40 +0100

    Style: Styled the third section in the homepage

--- src/components/homepage-section/Third.jsx
+++ src/components/homepage-section/Third.jsx
@@ -1,34 +1,45 @@
 import React from 'react'
 import "../../styles/Third.scss"
+import Image from "../../assets/a.jpg"
 
 function Third() {
   return (
     <div className='third'>
         <div className="holder">
             <main>
-                <div className="image"></div>
+                <div className="image"><img src={Image} alt="image" /></div>
+                <div className="hold">
                 <div className="text">
                     <h3>About Us</h3>
                     <hr />
-                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                 </div>
-                <div className="image"></div>
-                <div className="text">
-                    <h3>About Us</h3>
-                    <hr />
-                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
                 </div>
-                <div className="image"></div>
+                <div className="image"><img src={Image} alt="image" /></div>
+                <div className="hold">
                 <div className="text">
                     <h3>About Us</h3>
                     <hr />
-                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                 </div>
-                <div className="image"></div>
+                </div>
+            </main>
+            <main>
+                <div className="image"><img src={Image} alt="image" /></div>
+                <div className="hold">
                 <div className="text">
                     <h3>About Us</h3>
                     <hr />
-                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                </div>
+                </div>
+                <div className="image"><img src={Image} alt="image" /></div>
+                <div className="hold">
+                <div className="text">
+                    <h3>About Us</h3>
+                    <hr />
+                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                </div>
                 </div>
             </main>
         </div>
