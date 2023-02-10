e33457581aae4be232dd5a077fca98f301bddb06
oyinkansola <adebayoalameen88@gmail.com>
2023-02-10 22:02:50 +0100

    Style: Styled the second section in the homepage

--- src/components/homepage-section/Second.jsx
+++ src/components/homepage-section/Second.jsx
@@ -1,41 +1,49 @@
 import React from 'react'
 import "../../styles/Second.scss"
+import Image from "../../assets/a.jpg"
+import Middle from "../../assets/a.jpg"
 
 function Second() {
   return (
     <div className='second'>
         <div className="call"> CALL US +234 1234 56789</div>
         <div className="hold">
-            <div className="left">
+        <div className="left">
                 <div className="first">
                     <div className="a">
                     <h3>Architecture</h3>
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
+                    </div>
+                    <div className="first">
                     <div className="a">
                     <h3>Architecture</h3>
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
+                    </div>
+                    <div className="first">
                     <div className="a">
                     <h3>Architecture</h3>
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
+                    </div>
+                    <div className="first">
                     <div className="a">
                     <h3>Architecture</h3>
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
                     </div>
                 </div>
                 <div className="middle">
-
+                    <img src={Middle} alt="" />
                 </div>
                 <div className="right">
                 <div className="first">
@@ -44,25 +52,31 @@ function Second() {
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
+                    </div>
+                    <div className="first">
                     <div className="a">
                     <h3>Architecture</h3>
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
+                    </div>
+                    <div className="first">
                     <div className="a">
                     <h3>Architecture</h3>
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
+                    </div>
+                    <div className="first">
                     <div className="a">
                     <h3>Architecture</h3>
                     <hr />
                     <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
-                    <div className="b"></div>
+                    <div className="b"><img src={Image} alt="image" /></div>
                     </div>
                 </div>
         </div>
